bc8daa540dbfd2b3c8b8bde37af85532989f411f
Sebastian K. Sorensen <sks1993@gmail.com>
2021-02-11 20:39:07 +0100

    name help file

--- lint.lua
+++ lint.lua
@@ -1,11 +1,11 @@
-VERSION = "0.0.4"
+VERSION = "0.0.5"
 
 local config = import("micro/config")
 
 function init()
     linter.makeLinter("typescript", "typescript", "tsc", {"--noEmit"}, "%f(%l,%c): %m")
 
-    config.AddRuntimeFile("linter", config.RTHelp, "help/linter-typescript.md")
+    config.AddRuntimeFile("linter-typescript", config.RTHelp, "help/linter-typescript.md")
 
     -- lib/layout/Button.tsx(57,19): error TS2554: Expected 1 arguments, but got 0.
 end
