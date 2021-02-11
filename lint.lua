1105356eca91688c1e196ffe8bc3ac5908dedf83
Sebastian K. Sorensen <sks1993@gmail.com>
2021-02-11 21:32:40 +0100

    log

--- lint.lua
+++ lint.lua
@@ -1,8 +1,9 @@
-VERSION = "0.0.10"
+VERSION = "0.0.11"
 
 local config = import("micro/config")
 
 function init()
-    linter.makeLinter("typescript", "typescript", "tsc", {"--noEmit"}, "%f:%l:%c - %m")
+	micro.Log(("lintertypescript log: %s"):format("Typescript Linter started."))
+    linter.makeLinter("typescript", "typescript", "tsc", {}, "%f:%l:%c - %m")
     config.AddRuntimeFile("linter-typescript", config.RTHelp, "help/linter-typescript.md")
 end
