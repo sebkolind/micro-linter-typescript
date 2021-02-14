167728fd180746decc07ba9677ce69d70504f982
Sebastian K. Sorensen <sks1993@gmail.com>
2021-02-14 12:06:19 +0100

    remove typescriptreact and use typescript with update syntax

--- lint.lua
+++ lint.lua
@@ -4,6 +4,5 @@ local config = import("micro/config")
 
 function init()
     linter.makeLinter("tsc", "typescript", "tsc", {"--noEmit", "--pretty", "false"}, "%f%(%l,%c%): %m")
-    linter.makeLinter("tscreact", "typescriptreact", "tsc", {"--noEmit", "--pretty", "false"}, "%f%(%l,%c%): %m")
     config.AddRuntimeFile("lintertypescript", config.RTHelp, "help/linter-typescript.md")
 end
